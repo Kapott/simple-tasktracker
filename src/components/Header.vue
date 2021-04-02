1385da55bb1fd07283fca202d3f31e00372938b2
kapott <kapott@aivd.33mail.com>
2021-04-02 12:24:45 +0200

    Code. Tutorial style.

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add task" color="#e00" />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    Button,
+  },
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 1.5em;
+    }
+</style>
