c6e196582d4388f88374ca3d6e935e8255b210c5
kapott <kapott@aivd.33mail.com>
2021-04-02 15:27:42 +0200

    Add singular task fetch.

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,11 @@ export default {
       const data = await res.json();
       return data;
     },
+    async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`);
+      const data = await res.json();
+      return data;
+    },
   },
   async created() {
     this.tasks = await this.fetchTasks();
