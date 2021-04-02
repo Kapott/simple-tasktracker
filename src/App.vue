6263557492baaf049485514ad436888a4a624452
kapott <kapott@aivd.33mail.com>
2021-04-02 15:26:05 +0200

    Fetch data from backend please.

--- src/App.vue
+++ src/App.vue
@@ -47,34 +47,14 @@ export default {
       this.tasks = this.tasks
         .map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task));
     },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json();
+      return data;
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Gras maaien',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Onkruid uit gras steken',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Trap schuren',
-        day: 'March 1st at 2:30pm',
-        reminder: false,
-      },
-      {
-        id: 4,
-        text: 'Traptreden beitsen',
-        day: 'March 1st at 2:30pm',
-        reminder: false,
-      },
-    ];
+  async created() {
+    this.tasks = await this.fetchTasks();
   },
 };
 </script>
